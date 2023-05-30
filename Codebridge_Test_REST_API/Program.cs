3b76ac24d11b9674bda97f9b511d1d2bd5d2dd95
Vitalii Khrystov <vitalik_khristov@ukr.net>
2023-05-30 16:16:26 +0300

    Add project files.

---
+++ Codebridge_Test_REST_API/Program.cs
@@ -0,0 +1,15 @@
+namespace Codebridge_Test_REST_API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
